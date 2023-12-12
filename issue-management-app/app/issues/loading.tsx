7d95144c71526c0546f2adb1716269590e492ba6
jos50275266 <jos50275266@gmail.com>
2023-12-12 11:30:51 +0900

    26. 패키지 리팩토링

--- issue-management-app/app/issues/loading.tsx
+++ issue-management-app/app/issues/loading.tsx
@@ -1,8 +1,8 @@
 import { Table } from "@radix-ui/themes";
 import React from "react";
-import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import IssueActions from "./IssueActions";
+import Skeleton from "../components/Skeleton";
 
 const LoadingIssuesPage = () => {
   // 임시 데이터
