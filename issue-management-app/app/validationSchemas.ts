3eb1be3957ade916ded2082c84a9b8e122f9d1ff
jos50275266 <jos50275266@gmail.com>
2023-12-13 19:42:53 +0900

    30. 이슈 업데이트 API 구축

--- issue-management-app/app/validationSchemas.ts
+++ issue-management-app/app/validationSchemas.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const createIssueSchema = z.object({
+export const issueSchema = z.object({
   title: z.string().min(1, "글자가 짧아요").max(255),
   description: z.string().min(1),
 });
