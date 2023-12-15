407b4cc3fb7e9f98a998b8adeba5274d961275a4
jos50275266 <jos50275266@gmail.com>
2023-12-15 16:01:59 +0900

    42. Prisma Adapter 추가

--- issue-management-app/app/api/auth/[...nextauth]/route.ts
+++ issue-management-app/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,19 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/client";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
 });
 
 export { handler as GET, handler as POST };
