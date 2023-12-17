e7412bd0576fa91b09c6ad7cb0cd071b0fb7f51c
jos50275266 <jos50275266@gmail.com>
2023-12-18 03:53:52 +0900

    49. 애플리케이션 보안(middleware)

--- issue-management-app/app/issues/[id]/page.tsx
+++ issue-management-app/app/issues/[id]/page.tsx
@@ -9,12 +9,15 @@ import Link from "next/link";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
 import DeleteIssueButton from "./DeleteIssueButton";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 interface Props {
   params: { id: string };
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
+  const session = await getServerSession(authOptions);
   const numericId = parseInt(params.id, 10);
 
   if (isNaN(numericId) || numericId.toString() !== params.id) {
@@ -31,12 +34,14 @@ const IssueDetailPage = async ({ params }: Props) => {
       <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
-      <Box>
-        <Flex direction="column" gap="4">
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>
+      {session && (
+        <Box>
+          <Flex direction="column" gap="4">
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
   );
 };
